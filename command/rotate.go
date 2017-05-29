dbdb4fd363794243d421345437fd970a7f69c05f
Yuichi Saito <munisystem@gmail.com>
2017-05-29 18:29:44 +0900

    Implement config loader

--- command/rotate.go
+++ command/rotate.go
@@ -2,6 +2,10 @@ package command
 
 import (
 	"strings"
+	"os"
+	"fmt"
+	"errors"
+	"github.com/munisystem/rstack/deployment"
 )
 
 type RotateCommand struct {
@@ -9,7 +13,28 @@ type RotateCommand struct {
 }
 
 func (c *RotateCommand) Run(args []string) int {
-	// Write your code here
+	if len(args) == 0 {
+		fmt.Fprintln(os.Stderr, errors.New("too few arguments"))
+		return 1
+	} else if len(args) > 1 {
+		fmt.Fprintln(os.Stderr, errors.New("too many arguments"))
+		return 1
+	}
+
+	name := args[0]
+
+	bucket := os.Getenv("AWS_S3_BUCKET_NAME")
+	if bucket == "" {
+		fmt.Fprintln(os.Stderr, errors.New("Please set s3 bucket name in AWS_S3_BUCKET_NAME"))
+		return 1
+	}
+
+	dep, err := deployment.Load(bucket, name)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+	fmt.Println(dep)
 
 	return 0
 }
